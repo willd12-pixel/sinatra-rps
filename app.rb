12b532a89f1310adea445ede25338679bbff5b51
willd12-pixel <143421896+willd12-pixel@users.noreply.github.com>
2024-02-21 15:35:28 +0000

    plays rock

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
 end
+
+ get("/rock") do  
+  move1 = ["rock" , "paper" , "scissors"]
+  @they1 = move1.sample
+  erb(:erb) 
+ end
+
+
+ end
